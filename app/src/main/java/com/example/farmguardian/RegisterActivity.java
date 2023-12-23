8dd9340a079b430de0cdf0fcc8461caf7e1eebd1
Bongani-4 <njathubongani@gmail.com>
2023-12-24 00:03:40 +0200

    Login and first page v2

--- app/src/main/java/com/example/farmguardian/RegisterActivity.java
+++ app/src/main/java/com/example/farmguardian/RegisterActivity.java
@@ -42,7 +42,7 @@ public class RegisterActivity extends AppCompatActivity {
                 String password = edpassword.getText().toString();
                 String email = edEmail.getText().toString();
                 String confirmpass = Edconfirm.getText().toString();
-
+                Database db = new Database(getApplicationContext(),"FarmGuardian",null,1);
                 if(username.length()==0 || password.length()==0 || email.length()==0 || confirmpass.length()==0)
                 {
                     Toast.makeText(getApplicationContext(),"All details should be filled!", Toast.LENGTH_SHORT).show();
@@ -50,12 +50,22 @@ public class RegisterActivity extends AppCompatActivity {
                 else{
                     if(password.compareTo(confirmpass)==0)
                     {
+                        if(Isvalid(password)) {
+
+                               db.register(username,email,password);
+                                Toast.makeText(getApplicationContext(), "Registration success", Toast.LENGTH_SHORT).show();
+                                 startActivity( new Intent( RegisterActivity.this,LoginActivity.class));
+                        }
+                        else{
+                            Toast.makeText(getApplicationContext(), "Passwords must contain  at least 8 characters,having letter,digit & special character", Toast.LENGTH_SHORT).show();
+                        }
+
 
                     }else{
                         Toast.makeText(getApplicationContext(), "Passwords don't match", Toast.LENGTH_SHORT).show();
                     }
 
-                    //Toast.makeText(getApplicationContext(), "Registration success", Toast.LENGTH_SHORT).show();
+
                 }
                 startActivity(new Intent(RegisterActivity.this,HomeActivity.class));
 
@@ -63,5 +73,45 @@ public class RegisterActivity extends AppCompatActivity {
         });
 
 
+    }
+    public static boolean Isvalid(String password) {
+        int F1 = 0, F2 = 0, F3 = 0;
+
+        if (password.length() < 8) {
+            return false;
+        } else {
+            for(int a = 0; a < password.length(); a++) {
+                if(Character.isLetter(password.charAt(a))) {
+                    F1=1;
+                }
+            }
+            for(int K =0;K<password.length();K++)
+            {
+                if(Character.isDigit(password.charAt(K)))
+                {
+                    F2=1;
+                }
+
+
+            }
+            for(int b =0;b<password.length();b++)
+            {
+
+                char c = password.charAt(b);
+
+                if(c >= 33 && c<=46 ||c==64)
+                {
+                    F3=1;
+                }
+
+
+            }
+            if(F1==1 && F2==2 && F3==1) {
+                return true;
+            }
+
+
+        }
+        return false;
     }
 }
