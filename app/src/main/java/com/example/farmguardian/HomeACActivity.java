e8755af03367cc984a79f2e1a122969e13fc235b
Bongani-4 <njathubongani@gmail.com>
2023-12-27 21:00:14 +0200

    simplify user navigation

--- app/src/main/java/com/example/farmguardian/HomeACActivity.java
+++ app/src/main/java/com/example/farmguardian/HomeACActivity.java
@@ -1,8 +1,10 @@
 package com.example.farmguardian;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeACActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class HomeACActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_acactivity);
+
+        CardView logout = findViewById(R.id.Logout);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeACActivity.this,FirstPageActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/farmguardian/HomeActivity.java
+++ app/src/main/java/com/example/farmguardian/HomeActivity.java
@@ -1,8 +1,11 @@
 package com.example.farmguardian;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        CardView exit = findViewById(R.id.Logout);
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeActivity.this,FirstPageActivity.class));
+            }
+        });
     }
 }
