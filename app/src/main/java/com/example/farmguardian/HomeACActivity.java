fd44609361da0e41c84bb59356ca8dfd9c91a3b5
Bongani-4 <njathubongani@gmail.com>
2023-12-27 21:01:10 +0200

    simplify user navigation

--- app/src/main/java/com/example/farmguardian/HomeACActivity.java
+++ app/src/main/java/com/example/farmguardian/HomeACActivity.java
@@ -3,6 +3,7 @@ package com.example.farmguardian;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
